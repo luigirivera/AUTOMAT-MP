4e99c5ac45fbef35bfce93a232c82c492968e108
Luigi Rivera <luigiriveraiv@gmail.com>
2018-08-08 08:59:28 +0800

    more automaton shit

--- src/view/Automaton.java
+++ src/view/Automaton.java
@@ -9,6 +9,21 @@ public class Automaton extends JFrame {
 	
 	public Automaton()
 	{
+		super("AUTOMAT Machine Project S18");
+		
+		setResizable(false);
+		setSize(500,500);
+		setLayout(null);
+		
+		
+		instantiate();
+		
+		setVisible(true);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+	}
+	
+	private void instantiate()
+	{
 		imagePanel = new ImagePanel();
 	}
 }
