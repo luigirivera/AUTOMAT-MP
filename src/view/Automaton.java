0ea91852e51335bce579e8e6e5997f64e47c265c
Luigi Rivera <luigiriveraiv@gmail.com>
2018-08-10 11:33:41 +0800

    Lol

--- src/view/Automaton.java
+++ src/view/Automaton.java
@@ -1,11 +1,13 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Automaton extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private ImagePanel imagePanel;
+	private JPanel highlightPanel;
 	
 	public Automaton()
 	{
@@ -26,12 +28,15 @@ public class Automaton extends JFrame {
 	private void instantiate()
 	{
 		imagePanel = new ImagePanel();
+		highlightPanel = new JPanel();
 	}
 	
 	private void initialize()
 	{
 		add(imagePanel);
+		add(highlightPanel);
 		
 		imagePanel.setBounds(0, 0, 1010, 568);
+		highlightPanel.setBounds(imagePanel.getBounds());
 	}
 }
