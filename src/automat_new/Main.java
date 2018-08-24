eb0808622502e4df43e969197223dc14d047730d
Brian Poblete <brian_poblete@dlsu.edu.ph>
2018-08-24 12:39:45 +0800

    Now prints out all transitions'

--- src/automat_new/Main.java
+++ src/automat_new/Main.java
@@ -1,6 +1,20 @@
 package automat_new;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String ... args){
         Automaton a = new Automaton();
+        DFS d = new DFS(a);
+        ArrayList<Transition> temp = new ArrayList<>();
+        for(int i = 0; i < d.getSolutionTransitions().size(); i++) {
+        	temp.addAll(d.getSolutionTransitions().get(i)); 
+            for(Transition t : temp) {
+            	System.out.print(t.getName() + "-> ");
+            }
+            System.out.println();
+            temp.clear();
+        }
+        
     }
 }
