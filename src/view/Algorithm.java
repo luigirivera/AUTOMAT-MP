8c8ebb5e2a82af73c2150cc2813e6e2ad35d1ba6
Luigi Rivera <luigiriveraiv@gmail.com>
2018-08-24 11:28:51 +0800

    with textarea

--- src/view/Algorithm.java
+++ src/view/Algorithm.java
@@ -9,9 +9,27 @@ public class Algorithm extends JFrame {
 	
 	public Algorithm()
 	{
+		super("AUTOMAT Machine Project S18 - Algorithm");
+		
+		instantiate();
+		setLayout(null);
+		initialize();
 		setSize(500,500);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
+	}
+	
+	private void instantiate()
+	{
+		textArea = new JTextArea();
+	}
+	
+	private void initialize()
+	{
+		add(textArea);
+		textArea.setBounds(0, 0, 500, 500);
+		
+		textArea.setEditable(false);
 	}
 }
